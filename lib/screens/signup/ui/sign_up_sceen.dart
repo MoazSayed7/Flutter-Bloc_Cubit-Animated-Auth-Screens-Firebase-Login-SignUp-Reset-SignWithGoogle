58e6d2f62e9a072141822e6fd0525f9857583a99
Moaz Sayed <75115429+MoazSayed7@users.noreply.github.com>
2024-02-23 11:10:38 +0200

    Update sign_up_sceen.dart

--- lib/screens/signup/ui/sign_up_sceen.dart
+++ lib/screens/signup/ui/sign_up_sceen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
+import 'package:signup_rest_login/helpers/google_sign_in.dart';
 
 import '../../../core/widgets/already_have_account_text.dart';
+import '../../../core/widgets/login_with_google.dart';
 import '../../../core/widgets/login_and_signup_animated_form.dart';
 import '../../../core/widgets/terms_and_conditions_text.dart';
 import '../../../theming/styles.dart';
@@ -36,7 +39,20 @@ class SignUpScreen extends StatelessWidget {
                     const EmailAndPassword(
                       isSignUpPage: true,
                     ),
-                    Gap(114.h),
+                    Gap(10.h),
+                    const LoginWithGoogle(),
+                    Gap(5.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: const FaIcon(FontAwesomeIcons.google),
+                          onPressed: () async {
+                            await GoogleSignin.signInWithGoogle(context);
+                          },
+                        ),
+                      ],
+                    ),
                     const TermsAndConditionsText(),
                     Gap(15.h),
                     const AlreadyHaveAccountText(),
