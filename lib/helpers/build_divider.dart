7eafa33a50a27c614ff4c05422dd32c37a61bb20
Moaz Sayed <75115429+MoazSayed7@users.noreply.github.com>
2024-02-23 11:02:06 +0200

    Add files via upload

---
+++ lib/helpers/build_divider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:signup_rest_login/theming/colors.dart';
+
+class BuildDivider {
+  static Widget buildDivider() {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        height: 2.h,
+        decoration: ShapeDecoration(
+          color: ColorsManager.gray,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/helpers/google_sign_in.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:signup_rest_login/helpers/extensions.dart';
+import 'package:signup_rest_login/routing/routes.dart';
+
+class GoogleSignin {
+  static Future signInWithGoogle(BuildContext context) async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    if (googleUser == null) {
+      return;
+    }
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    // Once signed in, return the UserCredential
+    await FirebaseAuth.instance.signInWithCredential(credential);
+    if (!context.mounted) return;
+    context.pushNamedAndRemoveUntil(
+      Routes.homeScreen,
+      predicate: (route) => false,
+    );
+  }
+}
