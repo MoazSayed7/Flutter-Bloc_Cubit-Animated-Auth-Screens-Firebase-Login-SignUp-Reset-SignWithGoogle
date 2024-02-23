bf73f97afe170c30b2f09a2a6783ff0944fc2c68
Moaz Sayed <75115429+MoazSayed7@users.noreply.github.com>
2024-02-23 11:19:45 +0200

    Update build_divider.dart

--- lib/helpers/build_divider.dart
+++ lib/helpers/build_divider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:signup_rest_login/theming/colors.dart';
+import '../theming/colors.dart';
 
 class BuildDivider {
   static Widget buildDivider() {
