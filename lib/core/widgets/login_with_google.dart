4dd68861e121e1a6717690f91b27d77944e0aadb
Moaz Sayed <75115429+MoazSayed7@users.noreply.github.com>
2024-02-23 11:01:22 +0200

    Add files via upload

---
+++ lib/core/widgets/login_with_google.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+
+import '../../theming/styles.dart';
+import '../../helpers/build_divider.dart';
+
+class LoginWithGoogle extends StatelessWidget {
+  const LoginWithGoogle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        BuildDivider.buildDivider(),
+        Gap(5.w),
+        Text(
+          'or Sign in with',
+          style: TextStyles.font13Grey400Weight,
+        ),
+        Gap(5.w),
+        BuildDivider.buildDivider(),
+      ],
+    );
+  }
+}
