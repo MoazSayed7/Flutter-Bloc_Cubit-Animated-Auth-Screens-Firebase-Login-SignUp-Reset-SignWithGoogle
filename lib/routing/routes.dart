862df0b7d3537665839c2bc7f85f609bea249881
Moaz Sayed <75115429+MoazSayed7@users.noreply.github.com>
2024-02-24 17:03:14 +0200

    add createPassword route

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -2,5 +2,6 @@ class Routes {
   static const String loginScreen = "/loginScreen";
   static const String forgetScreen = "/forgetScreen";
   static const String signupScreen = "/signupScreen";
+  static const String createPassword = "/createPassword";
   static const String homeScreen = "/homeScreen";
 }
