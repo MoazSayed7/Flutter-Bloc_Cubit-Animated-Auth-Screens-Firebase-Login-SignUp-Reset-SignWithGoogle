02af0d9607bd411c1c87a4ef8f5c82713448fae2
Moaz Sayed <75115429+MoazSayed7@users.noreply.github.com>
2024-02-24 17:08:19 +0200

    add check if new user or not

--- lib/helpers/google_sign_in.dart
+++ lib/helpers/google_sign_in.dart
@@ -1,31 +1,57 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'extensions.dart';
 import '../routing/routes.dart';
 
 class GoogleSignin {
-  static Future signInWithGoogle(BuildContext context) async {
-    // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-    if (googleUser == null) {
-      return;
-    }
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
+  static final _auth = FirebaseAuth.instance;
 
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    // Once signed in, return the UserCredential
-    await FirebaseAuth.instance.signInWithCredential(credential);
-    if (!context.mounted) return;
-    context.pushNamedAndRemoveUntil(
-      Routes.homeScreen,
-      predicate: (route) => false,
-    );
+  static Future signInWithGoogle(BuildContext context) async {
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) {
+        return;
+      }
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential authResult =
+          await _auth.signInWithCredential(credential);
+
+      if (authResult.additionalUserInfo!.isNewUser) {
+        await _auth.currentUser!.delete();
+        if (!context.mounted) return;
+        context.pushNamedAndRemoveUntil(
+          Routes.createPassword,
+          predicate: (route) => false,
+          arguments: [googleUser, credential],
+        );
+      } else {
+        if (!context.mounted) return;
+        context.pushNamedAndRemoveUntil(
+          Routes.homeScreen,
+          predicate: (route) => false,
+        );
+      }
+    } catch (e) {
+      await AwesomeDialog(
+        context: context,
+        dialogType: DialogType.info,
+        animType: AnimType.rightSlide,
+        title: 'Sign in error',
+        desc: e.toString(),
+      ).show();
+    }
   }
 }
